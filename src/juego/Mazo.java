aa49d3a7890a756a8e678b707ba43c558145891f
Suldrum <selenerodrigo@outlook.com>
2020-11-01 17:06:19 -0300

    comentarios

--- src/juego/Mazo.java
+++ src/juego/Mazo.java
@@ -65,22 +65,21 @@ public class Mazo {
 	}
 	
 	public void validarCartas() {
-		Carta primeraCarta = this.mazo.get(0);
+		Carta primeraCarta = this.mazo.get(0); //Desde la primera carta se valida los atributos
+		
 		// Guarda una lista de índices para buscar las cartas del mazo para borrar
 		ArrayList<Integer> cartasExtranias = new ArrayList<>();
 		
-		for(int i = 1; i < this.mazo.size(); i++) {
+		for(int i = 1; i < this.mazo.size(); i++) { // itera por todas las cartas del mazo
 			Carta cartaActual = this.mazo.get(i);
-			if(cartaActual.cantidadAtributos() == primeraCarta.cantidadAtributos()) {
+			if(cartaActual.cantidadAtributos() == primeraCarta.cantidadAtributos()) { //primero se fija que coincida la cantidad de atributos, de no hacerlo ya es rechazada
 				int j = 0;
 				Atributo aux = cartaActual.getAtributo(j);
-				
-				
-				while((j < cartaActual.cantidadAtributos()) && (primeraCarta.getAtributo(aux.getNombre()) != null)) {
+				while((j < cartaActual.cantidadAtributos()) && (primeraCarta.getAtributo(aux.getNombre()) != null)) { //Verifica que todos los atributos de la carta esten en la primera carta
 					aux = cartaActual.getAtributo(j);
 					j++;
 				}
-				if(j < cartaActual.cantidadAtributos()) {
+				if(j < cartaActual.cantidadAtributos()) { //Si no los encontro a todos es rechazada
 					cartasExtranias.add(i);
 				}
 			}
@@ -88,7 +87,6 @@ public class Mazo {
 				cartasExtranias.add(i);
 		}
 		
-		System.out.println("Cantidad de cartas extrañas:"+cartasExtranias.size());
 		for(int i = 0; i < cartasExtranias.size(); i++) {
 			this.mazo.remove((int)cartasExtranias.get(i));
 		}
